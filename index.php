aacdec09319ef6fb5985b0f40cc59d167ca77a5e
ariful59 <arifulamindu@gmail.com>
2017-05-19 11:30:13 +0600

    mobile view

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
-<html class="no-js"> 
+<html class="no-js">
 <html lang="en">
+
 <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
     <title>DU SPORTS</title>
     <link href="css/bootstrap.css" rel="stylesheet">
