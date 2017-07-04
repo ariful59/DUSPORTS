7b024631c783b6aa2924019df4cc2b281d0ab19c
Ariful Amin <arifulamindu@gmail.com>
2017-07-04 18:20:25 +0600

    Add files via upload

---
+++ getData.php
@@ -0,0 +1,30 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+
+    $id=$_GET['id'];
+
+    //$conn = new mysqli('localhost', 'root', '', 'WebProject');
+	$conn = new mysqli('localhost', 'dusports', 'dusports', 'dusports');
+
+    $sql = "SELECT * FROM colleges WHERE id='".$id."'";
+
+    $r = mysqli_query($conn,$sql);
+
+    $res = mysqli_fetch_array($r);
+
+    $result = array();
+
+    array_push($result,array(
+            "name"=>$res['name'],
+            "address"=>$res['address'],
+            "vc"=>$res['vicechancellor']
+        )
+    );
+
+    echo json_encode(array("result"=>$result));
+
+    mysqli_close($conn);
+
+}
+?>
