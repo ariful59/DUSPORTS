a55893577ea7b63470a04abce482bb01feb53767
Ariful Amin <arifulamindu@gmail.com>
2017-07-05 16:09:00 +0600

    Add files via upload

---
+++ getData.php
@@ -0,0 +1,36 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+
+    $id=$_GET['id'];
+
+   // $conn = new mysqli('localhost', 'root', '', 'webproject');
+	$conn = new mysqli('localhost', 'dusports', 'dusports', 'dusports');
+    mysqli_query($conn,"SET CHARACTER SET utf8");
+    mysqli_query($conn,"SET SESSION collation_connection ='utf8_general_ci'") or die (mysqli_error());
+    mysqli_query($conn,"SET CHARACTER_SET_RESULTS = utf8");
+    $sql = "SELECT * FROM contact WHERE name like '%".$id."%'";
+    $r = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+
+    $res = mysqli_fetch_array($r);
+
+    $result = array();
+
+    array_push($result,array(
+            "name"=>$res['name'],
+            "address"=>$res['address'],
+            "email"=>$res['email'],
+            "phone_number"=>$res['phone_number'],
+            "university"=>$res['university'],
+            "dept_name"=>$res['dept_name'],
+            "year"=>$res['year'],
+            "blood_group"=>$res['blood_group']
+        )
+    );
+
+    echo json_encode(array("result"=>$result));
+
+    mysqli_close($conn);
+
+}
+?>
